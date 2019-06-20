ff6d164bc4e3f89e7b28759326a58416dc266f58
Jafar Khoshtabiat <jafar.khoshtabiat@gmail.com>
2019-06-21 01:16:03 +0430

    app working in background mode

--- IOS Reliable Messaging Challenge/AppDelegate.swift
+++ IOS Reliable Messaging Challenge/AppDelegate.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var messageLibrary: ReliableMessagingLibrary?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -25,8 +26,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
